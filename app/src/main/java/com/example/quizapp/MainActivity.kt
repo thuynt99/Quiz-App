55cb420979e5d9e77e968dacd6549b7e2be4c284
Nguyễn Thị Thúy <thuynguyen03091999@gmail.com>
2021-11-23 09:58:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val etName = findViewById<AppCompatEditText>(R.id.et_name)
+        btnStart.setOnClickListener{
+            if(etName.text.toString().isEmpty()) {
+
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
